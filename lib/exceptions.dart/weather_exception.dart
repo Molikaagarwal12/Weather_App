9a572dd1ca8f56784f62e45744c43dd1f4029300
Molikaagarwal12 <molikaagarwal2313@gmail.com>
2023-06-11 15:22:42 +0530

    weather_app

---
+++ lib/exceptions.dart/weather_exception.dart
@@ -0,0 +1,11 @@
+class WeatherException {
+  String message;
+   WeatherException([this.message='Something went wrong']){
+    message = 'Weather Exception: $message';
+  }
+
+@override
+String toString() {
+    return message;
+  }
+}
